83756469ed3957d2b318ba58d07784964fb9d099
Willy-WZW <joystick449@gmail.com>
2024-11-23 17:06:46 +0800

    login page update

---
+++ src/components/Login.vue
@@ -0,0 +1,84 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        temporaryBTN() {
+            this.$router.push("./socialPage")
+        },
+    },
+}
+</script>
+
+<template>
+    <div class="loginBox">
+        <h1>登入帳號</h1>
+        <div class="signIn">
+            <span>第一次登入&nbsp>>&nbsp</span>
+            <a href="">註冊</a>
+        </div>
+        <div class="phoneNum">
+            <h3>電話號碼</h3>
+            <input type="text" placeholder="請輸入電話號碼">
+        </div>
+        <div class="password">
+            <h3>密碼</h3>
+            <input type="password" placeholder="請輸入密碼">
+        </div>
+        <button class="loginBTN">登入</button>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.loginBox {
+    width: 100%;
+    height: 55dvh;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-direction: column;
+    background-color: white;
+    border: 1px solid black;
+    border-radius: 10px;
+
+    .signIn {
+        width: 100%;
+        font-size: 18px;
+        letter-spacing: 2px;
+        display: flex;
+        justify-content: center;
+    }
+
+    .phoneNum,
+    .password {
+        width: 60%;
+        letter-spacing: 2px;
+
+        h3 {
+            margin: 2% 0;
+        }
+
+        input {
+            width: 100%;
+            font-size: 20px;
+            padding: 3%;
+            background-color: rgba(147, 147, 147, 0.3);
+            border: none;
+            border-radius: 5px;
+        }
+    }
+
+    .loginBTN {
+        width: 60%;
+        margin-top: 3%;
+        color: white;
+        background-color: black;
+        font-size: 20px;
+        border-radius: 5px;
+    }
+
+}
+</style>
--- src/views/LogInPage.vue
+++ src/views/LogInPage.vue
@@ -1,4 +1,5 @@
 <script>
+import Login from '@/components/Login.vue'
 export default {
   data() {
     return {
@@ -9,6 +10,9 @@ export default {
     temporaryBTN() {
       this.$router.push("./socialPage")
     },
+  },
+  components:{
+    Login
   }
 }
 </script>
@@ -16,40 +20,13 @@ export default {
 <template>
   {{ "這邊做登入頁面" }}
   <button @click="temporaryBTN()">社群頁面</button>
-  <div class="signInArea">
-    <h1>登入帳號</h1>
-    <div class="signIn">
-      <span>第一次登入>></span>
-      <a href="">註冊</a>
-    </div>
-    <div class="phoneNum">
-      <h3>電話號碼</h3>
-      <input type="text" placeholder="請輸入電話號碼">
-    </div>
-    <div class="password">
-      <h3>密碼</h3>
-      <input type="password" placeholder="請輸入密碼">
-    </div>
-    <button>登入</button>
+  <div class="loginArea">
+    <Login />
   </div>
 </template>
 
 <style scoped lang="scss">
-.signInArea {
+.loginArea {
   width: 25%;
-  height: 55dvh;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  flex-direction: column;
-  background-color: white;
-  border: 1px solid black;
-  border-radius: 10px;
-
-  .signIn {}
-
-  .phoneNum {}
-
-  .password {}
 }
 </style>

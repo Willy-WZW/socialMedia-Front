b419c777470305c9f804d994726a436bc4321f85
Willy-WZW <joystick449@gmail.com>
2024-11-23 17:33:10 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LogInPage from '../views/LogInPage.vue'
+import LoginPage from '../views/LogInPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'logIn',
-      component: LogInPage
+      name: 'login',
+      component: LoginPage
     },
     {
       path: '/socialPage',
       name: 'socialPage',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/SocialPage.vue')
-    }
+    },
+    {
+      path: '/signInPage',
+      name: 'signInPage',
+      component: () => import('../views/SignInPage.vue')
+    },
   ]
 })
 
